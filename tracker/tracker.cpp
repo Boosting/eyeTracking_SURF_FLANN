6144043cdd29b46e6d201068cbbce2a2791f2ab2
Daniel Kneipp <dkdsv@hotmail.com>
2014-07-11 18:02:42 -0300

    Update tracker.cpp
    Descrito a sequência de passos para as funções haveEyesInFrame() e getEyesPosition()

--- tracker/tracker.cpp
+++ tracker/tracker.cpp
@@ -1,5 +1,28 @@
 #include "tracker.h"
 
-Tracker::Tracker()
+eyeT::Tracker::Tracker() : eyesFound( false ), eyesPositionFound( false ), K( 150 )
+{}
+
+//-> eyeTemplate deve ser guardado dentro da classe, e setado separadamente,
+//-> quando for setado, deve-se obter sues descritores automaticamente.
+bool eyeT::Tracker::haveEyesInFrame( cv::Mat& frame, cv::Mat& eyeTemplate )
 {
+    // Verificar se o template foi processado
+    
+    // Obter descritores do frame e do template (caso ainda nao tenha sido obtido) usando SURF
+    
+    // Comparar os descritores usando FLANN
+    
+    // Verificar o numero de bons pares de descritores (true, false)
+}
+
+cv::RotatedRect eyeT::Tracker::getEyesPosition( cv::Size& eyesFrameSize )
+{
+    // Verificar se jah foi processado o frame e achados os olhos
+    
+    // Obter matriz de homografia
+    
+    // Fazer a transformacao de perspectiva
+    
+    // Obter o retangulo rotacionado e retorna-lo
 }
