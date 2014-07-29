0458d568dc60e9e8752153283713dbc454482137
Daniel Kneipp <dkdsv@hotmail.com>
2014-07-29 19:36:53 -0300

    Construção da classe ORB.

--- ORB/orb.cpp
+++ ORB/orb.cpp
@@ -1,5 +1,28 @@
 #include "orb.h"
 
-ORB::ORB()
+eyeT::ORB::ORB()
+{}
+
+void eyeT::ORB::find_descriptors(cv::Mat &img)
 {
+    detector.detect (img, this->keyPnts);
+    extractor.compute (img, this->keyPnts, this->descriptors);
+}
+
+cv::Mat eyeT::ORB::get_descriptors_image()
+{
+    if (this->descriptors.empty())
+        return cv::Mat();
+
+    return this->descriptors;
+}
+
+int eyeT::ORB::get_keyPoints_number()
+{
+    return this->keyPnts.size();
+}
+
+std::vector< cv::KeyPoint > eyeT::ORB::get_keyPoints()
+{
+    return this->keyPnts;
 }
--- ORB/orb.h
+++ ORB/orb.h
@@ -1,11 +1,61 @@
 #ifndef ORB_H
 #define ORB_H
 
+#include <vector>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/features2d/features2d.hpp>
+
 namespace eyeT {
     class ORB
     {
+    private:
+        /** Detector dos pontos caracteristicos do frame. */
+        cv::OrbFeatureDetector detector;
+        /** Extrator dos descritores do frame (obtido com base nos KeyPoints). */
+        cv::OrbDescriptorExtractor extractor;
+        /** Estrutra que guarda os pontos caracteristicos detectados. */
+        std::vector< cv::KeyPoint > keyPnts;
+        /** Estrutura no formato de imagem (matriz) que representa os descritores detectados. */
+        cv::Mat descriptors;
+
     public:
         ORB();
+
+        /**
+         * @brief find_descriptors    Extrai os descritores de uma imagem passada
+         *                            como parametro e grava o resultado no atributo
+         *                            'descriptors'.
+         *
+         * @param img    Imagem que sera usada para calcular os descritores.
+         *
+         *
+         * @param keyPnts    Vector que será usado para guardar todos os
+         *                   pontos caracteristicos.
+         */
+        void find_descriptors (cv::Mat& img);
+
+        /**
+         * @brief get_descriptors_image     Pegar os descritores ddo frame processado.
+         *
+         * @return Uma imagem que representa os descritores do frame processado.
+         */
+        cv::Mat get_descriptors_image ();
+
+        /**
+         * @brief get_keyPoints_number  Pegar o numero de pontos caracteristicos detectados.
+         *
+         * @return quantidade de pontos caracteristicos (keypoints)
+         */
+        int get_keyPoints_number ();
+
+        /**
+         * @brief get_keyPoints     Pegar a estrutura que guarda os pontos
+         *                          caracteristicos detectados, e suas informacoes.
+         *
+         * @return std::vector de cv::Keypoint que contem os pontos caracteristicos detectados
+         */
+        std::vector< cv::KeyPoint > get_keyPoints ();
     };
 }
 
